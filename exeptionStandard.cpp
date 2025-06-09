55b68d140a42711cb0a7a71c87cd95e495fa7d92
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-06-09 15:24:45 +0700

    menambahkan exception try di exception standard

---
+++ exeptionStandard.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 10, 20, 30 };
+        //pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5 
+    }
+}
