9d7602701151af4a6681bd4fdef509c6b3603a1d
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-06-09 14:30:47 +0700

    menambahkan objek output file stream

--- ioFille.cpp
+++ ioFille.cpp
@@ -2,3 +2,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+
+    //membuat objek output file stream
+    ofstream outfile;
+    // membuka file untuk ditulisi
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+}
